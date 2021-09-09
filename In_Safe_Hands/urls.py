165499aba0b0bc1284d62b5f29a41c4442ce2140
smcgdub <stephen_xyz1@hotmail.com>
2021-09-09 19:06:31 +0000

    Have created the about us page and wired it up so its loading correctly. Ready to start adding content to it now

--- In_Safe_Hands/urls.py
+++ In_Safe_Hands/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('about_us/', include('about_us.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
