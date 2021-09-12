359e982304b084b47ecd69cf07d780b09a6cb54a
smcgdub <stephen_xyz1@hotmail.com>
2021-09-12 18:35:41 +0000

    Have added the search functionality to the search bar on thedesktop and mobile. Have manually tested and search functionality is working

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,6 +8,20 @@ def all_products(request):
 # View to return all of the products
 
     products = Product.objects.all()
+
+    # Search function if the user has submitted a search term/word 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            # If the user doesn't enter anything in the search box and presses search the homepage will reload
+            if not query:
+                messages.error(request, "You haven't entered anything to search for!")
+                return redirect(reverse('home'))
+
+            # Q allows to search for product by a product name or by a word in the description
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {'products': products,}
     return render(request, 'products/products.html', context)
 
