bc482c6b401431624b88b4a00012a646fc21bcb2
smcgdub <stephen_xyz1@hotmail.com>
2021-09-13 16:35:36 +0000

    Have included the product category so it is displayed for each product. Also removed the path('product_details/', include('products.urls')), code from the project level urls.py file as it was causing an error

--- products/views.py
+++ products/views.py
@@ -5,14 +5,29 @@ from .models import Product, Category
 
 
 def all_products(request):
-# View to return all of the products
+# View to return all of the products, search and sort queries also included
 
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     # For when users click on a specific category of items in the drop down menu 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey) 
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -30,10 +45,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
+        # 'categories': categories
         }
 
     return render(request, 'products/products.html', context)
@@ -43,5 +62,8 @@ def product_details(request, product_id):
 # View to return details on individual products
 
     product = get_object_or_404(Product, pk=product_id)
-    context = {'product': product,}
+    context = {
+        'product': product,
+        # 'categories': categories
+        }
     return render(request, 'products/product_details.html', context)
