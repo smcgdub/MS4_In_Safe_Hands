56d05cf1b345e06e32a25dd95123a87c6975f017
smcgdub <stephen_xyz1@hotmail.com>
2021-09-14 19:43:37 +0000

    Have updated the context.py file so items that are added to the bag are now displaying in the cart. Data only showing, need to create the tables and add css to tidy it up

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         # Always use DecimalField for money. Even simple operations like addition and subtraction are not immune to float rounding issues. https://stackoverflow.com/questions/2569015/django-floatfield-or-decimalfield-for-currency
--- cart/views.py
+++ cart/views.py
@@ -13,13 +13,12 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
     else:
-        bag[item_id] = quantity
+        cart[item_id] = quantity
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
+    request.session['cart'] = cart
     return redirect(redirect_url)
