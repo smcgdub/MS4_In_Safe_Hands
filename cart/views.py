ba5c35f3e6067eeaa2c4f53b901e5a6f08f9dea1
smcgdub <stephen_xyz1@hotmail.com>
2021-09-16 16:47:23 +0000

    Have added the update and remove options from the qty column in the cart.html  - The remove option is working as intended, the update is not and needs fixing

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
 def view_cart(request):
-  # A view that will render the content sof the users shopping cart
-
+  # A view that will render the contents of the users shopping cart
     return render(request, 'cart/cart.html')
 
 
@@ -22,3 +23,35 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    # Add the quantity of a specific item to the shopping cart
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    # if item_id in list(cart.keys()):
+    #     cart[item_id] += quantity
+    # else:
+    #     cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart' ))
+
+
+def remove_from_cart(request, item_id):
+    # To remove an item from the shopping cart
+
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
