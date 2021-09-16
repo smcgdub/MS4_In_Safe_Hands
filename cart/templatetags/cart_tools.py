92a14162e7eb289d3f79f414a340ad8a5b097d07
smcgdub <stephen_xyz1@hotmail.com>
2021-09-16 18:21:05 +0000

    Have added the templatetags folder and the cart_tools.py file for working out the subtotal of each row in the cart. Also adjusted the padding of each item row so the details are centered in the row

---
+++ cart/templatetags/cart_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    # This function is to return the subtotal of each row item in cart.html page
+    return price * quantity
