edbaf4bce028cc77d1cf7fef15491414083825a2
smcgdub <stephen_xyz1@hotmail.com>
2021-09-23 20:49:16 +0000

    Have created the payment payment details section in the checkout.html file.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,15 +1,16 @@
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements.create('card');
+var elements = stripe.elements();
+// Style of card input
 var style = {
   base: {
-    color: '#000',
+    color: '#212529',
     fontFamily: '"Josefin Sans", sans-serif',
     fontSmoothing: 'antialiased',
     fontSize: '16px',
     '::placeholder': {
-      color: '#aab7c4'
+      color: '#c3c1c1'
     }
   },
   invalid: {
@@ -19,4 +20,3 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
-
