f1bb122d0518b86a4ee7b989310767bcf7297dbe
smcgdub <stephen_xyz1@hotmail.com>
2021-09-28 17:06:55 +0000

    Have added the payment processing overlay to the checkout page

--- checkout/models.py
+++ checkout/models.py
@@ -18,7 +18,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
-    eircode = models.CharField(max_length=20, null=True, blank=True)
+    eircode = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
