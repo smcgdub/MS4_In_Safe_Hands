9bced284a30a3043ea010f41bf54989cb83c5daf
smcgdub <stephen_xyz1@hotmail.com>
2021-09-29 21:43:02 +0000

    Added address form to profile.html page and added profile to views.py in profiles

---
+++ profiles/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        # Fields to dispaly on the checkout form
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        # Placeholders to be added to the checkout fields
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_email': 'Email Address',
+            'default_phone_number': 'Phone Number',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County/Locality',
+            'default_eircode': 'Eircode/Postcode',
+            'default_country': 'Country',
+        }
+
+        # Form will auto start on first name
+        self.fields['default_email'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                # Asterisk will appear on required fields
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # Set placeholders 
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            # self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            # Labels set to false as they are not being used 
+            self.fields[field].label = False
--- profiles/models.py
+++ profiles/models.py
@@ -15,7 +15,7 @@ class UserProfile(models.Model):
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_eircode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Select Country *', null=True, blank=True)
+    default_country = CountryField(blank_label='Select Country', null=True, blank=True)
 
     def __str__(self):
             return self.user.username
