ff095fbbfca9c4fff8917ce3b30721f92cb7d696
smcgdub <stephen_xyz1@hotmail.com>
2021-10-20 13:49:31 +0000

    Have added the missing .parse to the DATABASE_URL that was missing on line 130 in the settings.py file

--- In_Safe_Hands/settings.py
+++ In_Safe_Hands/settings.py
@@ -127,14 +127,15 @@ WSGI_APPLICATION = 'In_Safe_Hands.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
