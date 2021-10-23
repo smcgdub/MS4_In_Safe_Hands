45dd68b05ee6a7c61f074d6b0d53656afc7356f1
smcgdub <stephen_xyz1@hotmail.com>
2021-10-23 14:57:17 +0000

    Have moved the confirmation emails folder to templates/checkout as emails were not sending

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -126,6 +126,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     first_name=shipping_details.name,
+                    # first_name=shipping_details.name,
                     last_name=shipping_details.name,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
