032902d7691627950f2be90ae19ce0b5bb49d8d3
smcgdub <stephen_xyz1@hotmail.com>
2021-10-25 17:11:10 +0000

    Have refactorded the code in the webhook_handler.py file on lines 73 and 76 as they were beyond the 79 character limit

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -70,8 +70,12 @@ class StripeWH_Handler:
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address1 = (
+                    shipping_details.address.line1
+                )
+                profile.default_street_address2 = (
+                    shipping_details.address.line2
+                )
                 profile.default_town_or_city = shipping_details.address.city
                 profile.default_county = shipping_details.address.state
                 profile.default_postcode = shipping_details.address.postal_code
